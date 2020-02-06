ec8a4c98ecb4e8332c4f4f2926b71c97c2f35ec1
Haohao Zhou <55772877+hzhou256@users.noreply.github.com>
2020-02-06 19:43:18 +0800

    2020/02/06

--- test.py
+++ test.py
@@ -1,57 +1,9 @@
-from sklearn.manifold import TSNE
+import xlrd
+import pandas as pd
 import numpy as np
-from sklearn import svm
-from sklearn import metrics
-from sklearn import model_selection
-from imblearn.metrics import specificity_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def get_feature(file):
-    m = np.shape(file)[0]
-    n = np.shape(file)[1]
-    data = np.zeros((m, n-1))
-    for index in range(m):
-        data[index] = file[index][1:]
-    return data
-
-
-dataset_name = ['Antifp_Main', 'Antifp_DS1', 'Antifp_DS2']
-methods_name = ['188-bit', 'AAC', 'ASDC', 'CKSAAP', 'DPC']
-alphabet = ['(A)', '(B)', '(C)', '(D)', '(E)']
-
-for ds in range(3):
-    name_ds = dataset_name[ds]
-    print(name_ds)
-    plt.figure(figsize = [12, 8])
-    for it in range(5):
-        name = methods_name[it]
-        print(name)
-        f1 = np.loadtxt('D:/Study/Bioinformatics/AFP/feature_matrix/' + name_ds + '/' + name +'/train_' + name +'.csv', delimiter = ',', skiprows = 1)
-        f2 = np.loadtxt('D:/Study/Bioinformatics/AFP/feature_matrix/' + name_ds + '/train_label.csv', delimiter = ',')
-
-        X_train = f1
-        y_train = f2
-        tsne=TSNE()
-        X_tsne = tsne.fit_transform(X_train)
-        print(X_tsne)
-
-        ax_train = plt.subplot(2, 3, it+1)
-        n_pos = int(sum(y_train))
-        n_neg = len(y_train) - n_pos
-        X_tsne_pos = np.zeros((n_pos, 2))
-        X_tsne_neg = np.zeros((n_neg, 2))
-        j, k = 0, 0
-        for i in range(len(y_train)):
-            if y_train[i] == 0:
-                X_tsne_neg[j] = X_tsne[i]
-                j = j + 1
-            else:
-                X_tsne_pos[k] = X_tsne[i]
-                k = k + 1
-
-        plt.scatter(X_tsne_pos[:, 0], X_tsne_pos[:, 1], c = 'c', marker = '.', s = 8, label = 'Pos')
-        plt.scatter(X_tsne_neg[:, 0], X_tsne_neg[:, 1], c = 'r', marker = '.', s = 8, label = 'Neg')
-        plt.legend(loc = 'upper left')
-        plt.title(alphabet[it] + '' + name, y = -0.18)
-    plt.savefig("D:/论文/图表/tsne可视化/" + name_ds + ".png")
-    plt.show()
+titanic = sns.load_dataset("titanic")
+print(titanic)
---
+++ 论文/氨基酸组成分布.py
@@ -0,0 +1,63 @@
+import xlrd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+
+AA = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 
+'M':10, 'N':11, 'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19}
+
+dataset_name = ['Antifp_Main', 'Antifp_DS1', 'Antifp_DS2']
+for ds in range(3):
+    name_ds = dataset_name[ds]
+    print('dataset:', name_ds)
+
+    train_file = "D:/Study/Bioinformatics/AFP/datasets/" + name_ds + "/" + name_ds + ".xlsx"
+    wb = xlrd.open_workbook(filename = train_file)
+    sheet1 = wb.sheet_by_name('positive')
+    sheet2 = wb.sheet_by_name('negative')
+
+    cols_positive = sheet1.col_values(1)
+    cols_negative = sheet2.col_values(1)
+    n_seq_pos = sheet1.nrows
+    n_seq_neg = sheet2.nrows
+    
+    sum_pos = 0
+    sum_neg = 0
+    temp_pos = np.zeros((20, 3))
+    temp_neg = np.zeros((20, 3))
+
+    for i in range(n_seq_pos):
+        sum_pos = sum_pos + len(cols_positive[i])
+        for x in cols_positive[i]:
+            if x == 'X':
+                continue
+            else:
+                temp_pos[AA[x]][0] = temp_pos[AA[x]][0] + 1
+    for j in range(n_seq_neg):
+        sum_neg = sum_neg + len(cols_negative[j])
+        for x in cols_negative[j]:
+            if x == 'X':
+                continue
+            else:
+                temp_neg[AA[x]][0] = temp_neg[AA[x]][0] + 1
+    for i in range(20):
+        temp_pos[i][0] = temp_pos[i][0] / sum_pos
+        temp_neg[i][0] = temp_neg[i][0] / sum_neg
+
+    result_pos = pd.DataFrame(temp_pos, columns = ['Composition', 'Type', 'Amino acid'])
+    result_neg = pd.DataFrame(temp_neg, columns = ['Composition', 'Type', 'Amino acid'])
+    result_pos['Type'] = pd.Series(['AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP', 'AFP'])
+    result_neg['Type'] = pd.Series(['Non_AFP', 'Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP','Non_AFP'])
+    result_pos['Amino acid'] = pd.Series(['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'])
+    result_neg['Amino acid'] = pd.Series(['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'])
+    result = pd.concat([result_pos, result_neg], ignore_index = True)
+
+    t, p = stats.ttest_ind(result_pos['Composition'], result_neg['Composition'])
+    #print(p)
+    sns.catplot(x = "Amino acid", y = "Composition", hue = "Type", kind = "bar", data = result, legend = False, height = 4.8, aspect = 2)
+    plt.legend()
+    #plt.savefig("D:/论文/图表/氨基酸组成分布图/" + name_ds + ".png")
+    plt.show()
