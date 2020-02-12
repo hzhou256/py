fc8fcb658ee3bd6b52ef19ce02f4ff4b1a60c5e5
Haohao Zhou <55772877+hzhou256@users.noreply.github.com>
2020-02-12 16:59:55 +0800

    Update tsne可视化.py

--- 论文/tsne可视化.py
+++ 论文/tsne可视化.py
@@ -23,20 +23,22 @@ for ds in range(3):
     for it in range(5):
         name = methods_name[it]
         print(name)
+        #读取特征文件，标签
         f1 = np.loadtxt('D:/Study/Bioinformatics/AFP/feature_matrix/' + name_ds + '/' + name +'/train_' + name +'.csv', delimiter = ',', skiprows = 1)
         f2 = np.loadtxt('D:/Study/Bioinformatics/AFP/feature_matrix/' + name_ds + '/train_label.csv', delimiter = ',')
 
-        X_train = get_feature(f1)
-        y_train = f2
+        X_train = get_feature(f1) #只取特征文件的矩阵部分
+        y_train = f2 #标签
         tsne=TSNE()
-        X_tsne = tsne.fit_transform(X_train)
+        X_tsne = tsne.fit_transform(X_train) #tsne降成2维
         print(X_tsne)
 
-        ax_train = plt.subplot(2, 3, it+1)
-        n_pos = int(sum(y_train))
-        n_neg = len(y_train) - n_pos
-        X_tsne_pos = np.zeros((n_pos, 2))
-        X_tsne_neg = np.zeros((n_neg, 2))
+        ax_train = plt.subplot(2, 3, it+1) #画子图
+        #将降维后的正负样本分开，方便画图（三类就分三个）
+        n_pos = int(sum(y_train)) #正样本数量
+        n_neg = len(y_train) - n_pos #负样本数量
+        X_tsne_pos = np.zeros((n_pos, 2)) #存储正样本
+        X_tsne_neg = np.zeros((n_neg, 2)) #存储负样本
         j, k = 0, 0
         for i in range(len(y_train)):
             if y_train[i] == 0:
@@ -45,10 +47,11 @@ for ds in range(3):
             else:
                 X_tsne_pos[k] = X_tsne[i]
                 k = k + 1
-
+        #正负分别画散点图
         plt.scatter(X_tsne_pos[:, 0], X_tsne_pos[:, 1], c = 'c', marker = '.', s = 8, label = 'Pos')
         plt.scatter(X_tsne_neg[:, 0], X_tsne_neg[:, 1], c = 'r', marker = '.', s = 8, label = 'Neg')
         plt.legend(loc = 'upper left')
+        #在图下面画标签
         plt.title(alphabet[it] + '' + name, y = -0.18)
     plt.savefig("D:/论文/图表/tsne可视化/" + name_ds + ".png")
     plt.show()
