eaca30e8d8261e2cd7e81d7ce2c7987aa634033e
Haohao Zhou <55772877+hzhou256@users.noreply.github.com>
2020-03-16 21:54:53 +0800

    Update membership.py

--- FuzzySVM/membership.py
+++ FuzzySVM/membership.py
@@ -2,8 +2,10 @@ import numpy as np
 from collections import Counter
 
 
-# Divide data set X into two subsets according to their labels y
 def split(X, y): 
+    '''
+    Divide dataset X into two subsets according to their labels y
+    '''
     cnt = Counter(y)
     n_pos = cnt[1] 
     n_neg = cnt[-1] 
@@ -20,7 +22,10 @@ def split(X, y):
     return X_pos, X_neg
 
 # 1. Use Class Center to Reduce the Effects of Outliers
-def class_center_membership(X, y, delta): # X: data, y: label, delta: parameter
+def class_center_membership(X, y, delta): 
+    '''
+    X: data, y: label, delta: parameter
+    '''
     X_pos, X_neg = split(X, y)
     mean_pos = np.mean(X_pos)
     mean_neg = np.mean(X_neg)
@@ -35,7 +40,10 @@ def class_center_membership(X, y, delta): # X: data, y: label, delta: parameter
     return s
 
 # 2. Fuzzy membership function for nonlinear SVM
-def get_radius_square(x_i, y_i, X, Y, K): # calculate distance between two points in feature space
+def get_radius_square(x_i, y_i, X, Y, K): 
+    '''
+    Calculate distance between two points in feature space
+    '''
     cnt = Counter(Y)
     n_pos = cnt[1]
     n_neg = cnt[-1]
@@ -48,7 +56,10 @@ def get_radius_square(x_i, y_i, X, Y, K): # calculate distance between two point
              + 1/(n_neg*n_neg) * np.sum([(K(X_neg[i], X_neg[j])) for i in range(n_neg) for j in range(n_neg)])
     return r_square
 
-def FSVM_2_membership(X, y, delta, K): # X: data, y: label, delta: parameter, K: kernel function
+def FSVM_2_membership(X, y, delta, K): 
+    '''
+    X: data, y: label, delta: parameter, K: kernel function
+    '''
     X_pos, X_neg = split(X, y)
     r_square_pos = np.max([(get_radius_square(x, 1, X, y, K)) for x in X_pos])
     r_square_neg = np.max([(get_radius_square(x, -1, X, y, K)) for x in X_neg])
@@ -61,7 +72,10 @@ def FSVM_2_membership(X, y, delta, K): # X: data, y: label, delta: parameter, K:
     return s
 
 # 3. Fuzzy SVM for Noisy Data
-def get_kth_evec(X, K, k, N): # calculate k_th largest eigenvector
+def get_kth_evec(X, K, k, N): 
+    '''
+    Calculate k_th largest eigenvector
+    '''
     G = np.zeros((N, N)) # Kernel matrix
     for i in range(N):
         for j in range(N):
@@ -71,14 +85,20 @@ def get_kth_evec(X, K, k, N): # calculate k_th largest eigenvector
     kth_evec = evecs[:,sorted_indices[-k]]
     return kth_evec
 
-def get_beta(x_i, X, K, N, j): # calculate beta_j
+def get_beta(x_i, X, K, N, j): 
+    '''
+    Calculate beta_j
+    '''
     alpha = get_kth_evec(X, K, j, N)
     beta = 0
     for i in range(N):
         beta = beta + alpha[i] * K(x_i, X[i])
     return beta
 
-def get_gamma(X, K, N, l): # calculate gamma_l
+def get_gamma(X, K, N, l): 
+    '''
+    Calculate gamma_l
+    '''
     alpha = get_kth_evec(X, K, l, N)
     gamma = 0
     for i in range(N):
@@ -87,7 +107,10 @@ def get_gamma(X, K, N, l): # calculate gamma_l
     gamma = gamma / N
     return gamma
 
-def reconstruction_error(x, X, y, K, k): # calculate reconstruction_error for x
+def reconstruction_error(x, X, y, K, k): 
+    '''
+    Calculate reconstruction_error for x
+    '''
     N = len(y)
     e_1 = K(x, x) - 2/N*np.sum([K(x, X[i]) for i in range(N)]) + 1/(N*N)*np.sum([K(X[i], X[j]) for i in range(N) for j in range(N)])
 
@@ -113,14 +136,21 @@ def reconstruction_error(x, X, y, K, k): # calculate reconstruction_error for x
     e = e_1 + e_2 + e_3
     return e
 
-def e_rescale(e, mu, sigma): # sigma: mean, mu: variance
+def e_rescale(e, mu, sigma): 
+    '''
+    Rescale the reconstruction_error
+    sigma: mean, mu: variance
+    '''
     temp = (e - mu) / sigma
     if temp > 0:
         return temp
     else:
         return 0
 
-def FSVM_N_membership(X, y, K, k, sigma_N): # X: data, y: label, K: kernel function, k: PCA dimension, sigma_N: parameter
+def FSVM_N_membership(X, y, K, k, sigma_N): 
+    '''
+    X: data, y: label, K: kernel function, k: PCA dimension, sigma_N: parameter
+    '''
     N = len(y)
     s = np.zeros(N)
     e = np.zeros(N)
