311dd36bf7361c41e06d9d4f1da185bad5976b5e
Haohao Zhou <55772877+hzhou256@users.noreply.github.com>
2020-03-17 18:45:54 +0800

    Create SVM_weight.py

---
+++ FuzzySVM/SVM_weight.py
@@ -0,0 +1,16 @@
+import sys
+path='D:/Program Files/libsvm_weights-3.23/python'
+sys.path.append(path)
+from svmutil import *
+
+
+y, x = svm_read_problem('D:/Program Files/libsvm_weights-3.23/heart_scale')
+W = [1] * len(y)
+W[0] = 10
+prob = svm_problem(W, y, x)
+param = svm_parameter('-s 3 -c 5 -h 0')
+m = svm_train([], y, x, '-c 5')
+m = svm_train(W, y, x)
+m = svm_train(prob, '-t 2 -c 5')
+m = svm_train(prob, param)
+CV_ACC = svm_train(W, y, x, '-v 3')
